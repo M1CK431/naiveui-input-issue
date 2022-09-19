42270077f2a408a08e102746a6fa52ba945cbaf2
M1CK431 <fontaine.mickael@laposte.net>
2022-09-19 17:02:19 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NConfigProvider :theme-overrides="theme">
+    <NInput style="width: 33%; margin: 2rem" />
+  </NConfigProvider>
+</template>
+
+<script setup>
+import { NConfigProvider, NInput, NButton } from 'naive-ui';
+
+const theme = {
+  common: { primaryColor: "hsl(350deg 78% 56%)" }
+};
+</script>
